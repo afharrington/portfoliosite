b787d8d1f8f2a269cc8355ea77aa02cafab8dfe7
Anna Harrington <annafkh@gmail.com>
2017-07-27 14:31:38 -0400

    Made card responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import '../css/main.scss';
+import './jquery.inview.min.js';
 
 $('.page-link').click(function(e){
   e.preventDefault();
   let go = this.getAttribute('href');
+
+  // if go == #about cause delay and then trigger about action
+
   $('.card-container').removeClass('slideDown').addClass('slideUp');
   setTimeout(function(){
     window.location = go;
@@ -13,4 +17,17 @@ $('.page-link').click(function(e){
 $('.nav-arrow').click(function(e){
   let go = this.getAttribute('href');
   $('.card-container').addClass('slideDown');
+});
+
+$("#about").bind("inview", function(isVisible) {
+  // Event is triggered once the element becomes visible in the browser's viewport, and once when it becomes invisible
+  if (isVisible) {
+    $( "#technical").fadeIn(800, () => {
+      $("#creative").fadeIn(800, () => {
+        $("#strategic").fadeIn(800, () => {
+          $("#teamoriented").fadeIn(800);
+        });
+      });
+    });
+  }
 });
